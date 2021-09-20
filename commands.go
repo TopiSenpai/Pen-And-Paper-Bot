7c15e622c0f95debeb1995c68aa7e0a3f246563f
Tobias Schultheiß <ts@vioma.de>
2021-09-20 17:51:24 +0200

    sum more progress

--- commands.go
+++ commands.go
@@ -6,31 +6,50 @@ import (
 )
 
 func onSoundAddCommand(event *core.SlashCommandEvent) {
-	_ = event.DeferCreate(true)
+	err := event.DeferCreate(true)
+	if err != nil {
+		event.Bot().Logger.Error("failed to DeferCreate(true) interaction: ", err)
+	}
 
-	soundName := event.Options["name"].String()
-
-	query := event.Options["source"].String()
+	name := event.Options["name"].String()
+	source := event.Options["source"].String()
+	query := source
 
 	// TODO: ytsearch: if no link or so
 
 	dgolink.RestClient().LoadItemHandler(query, api.NewResultHandler(
 		func(track api.Track) {
-			tr
+			addNewSound(event, name, track)
 		},
 		func(playlist *api.Playlist) {
-
+			track := playlist.SelectedTrack()
+			if track == nil {
+				track = playlist.Tracks[0]
+			}
+			addNewSound(event, name, track)
 		},
 		func(tracks []api.Track) {
-
+			addNewSound(event, name, tracks[0])
 		},
 		func() {
-			event
+			_, _ = event.UpdateOriginal(core.NewMessageUpdateBuilder().SetContentf("no track for `%s` found", source).Build())
 		},
 		func(e *api.Exception) {
-
+			_, _ = event.UpdateOriginal(core.NewMessageUpdateBuilder().SetContentf("error while getting track for `%s`", source).Build())
 		}),
 	)
+}
+
+func addNewSound(event *core.SlashCommandEvent, name string, track api.Track) {
+	if _, ok := sounds[*event.GuildID]; !ok {
+		sounds[*event.GuildID] = []Sound{}
+	}
+
+	sounds[*event.GuildID] = append(sounds[*event.GuildID], Sound{
+		Name:    name,
+		Track64: track.Track(),
+	})
+	_, _ = event.UpdateOriginal(core.NewMessageUpdateBuilder().SetContentf("added new sound `%s` with [source](%s)", name, track.Info().URI()).Build())
 }
 
 func onSoundRemoveCommand(event *core.SlashCommandEvent) {
--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module github.com/TopiSenpai/SoundBoard-Bot
 
 go 1.16
 
+replace (
+	github.com/DisgoOrg/disgolink => ../disgolink
+)
+
 require (
 	github.com/DisgoOrg/disgo v0.5.12-0.20210918181853-f7cd2b5ba91e
 	github.com/DisgoOrg/disgolink v0.2.5-0.20210919155345-1f0af3080486
